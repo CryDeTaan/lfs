cce66e61e8e09991e6724569b7583a2de7cd9ec4
CryDeTaan <CryDeTaan@users.noreply.github.com>
2026-02-19 18:28:27 +0000

    formatted

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,17 +2,21 @@
 
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $title ? $title . ' - ' : '' }}{{ config('app.name', 'Laravel') }}</title>
-</head>
-<body>
-    <nav>
-        <a href="{{ route('home') }}">Home</a>
-        <a href="{{ route('about') }}">About Us</a>
-        <a href="{{ route('contact') }}">Contact Us</a>
-    </nav>
-    {{ $slot }}
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>
+            {{ $title ? $title.' - ' : '' }}{{ config('app.name', 'Laravel') }}
+        </title>
+    </head>
+    <body>
+        <nav
+            class="navbar navbar-expand-lg navbar-light bg-light mb-5 flex flex-col justify-between pt-3 sm:flex-row"
+        >
+            <a href="{{ route('home') }}">Home</a>
+            <a href="{{ route('about') }}">About Us</a>
+            <a href="{{ route('contact') }}">Contact Us</a>
+        </nav>
+        {{ $slot }}
+    </body>
 </html>
